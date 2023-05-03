91ff554f1256249fa26d15b0e6860077db054557
Milo <milagros.camila.arana@gmail.com>
2023-05-03 19:44:39 -0300

    registro.java

--- src/Registro.java
+++ src/Registro.java
@@ -3,6 +3,18 @@ import javax.swing.*;
 public class Registro {
 
     private JPanel PanelRegistro;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JPasswordField passwordField1;
+    private JCheckBox empleadoCheckBox;
+    private JCheckBox proovedorCheckBox;
+    private JCheckBox clienteCheckBox;
+    private JComboBox <String> comboBox1;
+    private JButton confirmarButton;
+    private JLabel PRUEBA;
 
     public void setVisible(boolean b) {  //Esto es para que se conecte al otro form y sea visible
         JFrame frame = new JFrame("Registro");
