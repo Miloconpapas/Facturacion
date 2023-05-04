5ebefc7eb9deed7035623c348de0f3ba0bc5812b
Milo <milagros.camila.arana@gmail.com>
2023-05-03 22:29:02 -0300

    Agregado ciudades en Combobox

--- src/Registro.java
+++ src/Registro.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class Registro {
 
@@ -15,6 +18,16 @@ public class Registro {
     private JComboBox <String> comboBox1;
     private JButton confirmarButton;
     private JLabel PRUEBA;
+
+    public Registro(){
+        List<String> ciudades = Arrays.asList("Adolfo Gonzales Chaves","Adrogué","Alberti","Alejandro Korn","América Arrecifes", "Avellaneda", "Ayacucho", "Azul", "Bahía Blanca", "Balcarce", "Banfield", "Baradero", "Batán", "Béccar", "Bella Vista", "Benavídez", "Benito Juárez", "Berazategui", "Bernal", "Bolívar", "Bosques", "Boulogne Sur Mer", "Bragado", "Brandsen", "Burzaco", "Campana", "Cañuelas", "Capilla del Señor", "Capitán Sarmiento","Carhué", "Carlos Casares", "Carlos Spegazzini", "Carlos Tejedor", "Carmen de Areco", "Carmen de Patagones", "Casbas", "Caseros", "Castelar", "Castelli", "Chacabuco", "Chivilcoy", "Claypole", "Colón", "Comandante Nicanor Otamendi", "Coronel Dorrego", "Coronel Pringles", "Coronel Suárez", "Coronel Vidal", "Daireaux", "Dock Sud");
+        for (String ciudad : ciudades) {  // es más conveniente utilizar un bucle para agregar los elementos
+            comboBox1.addItem(ciudad); // la lista de ciudades se almacena en una variable ciudades y se utiliza un
+                                       // bucle for-each para iterar sobre cada elemento y agregarlo al JComboBox.
+        }
+    }
+
+
 
     public void setVisible(boolean b) {  //Esto es para que se conecte al otro form y sea visible
         JFrame frame = new JFrame("Registro");
